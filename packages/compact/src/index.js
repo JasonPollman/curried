94622e011e774837c6d0a73c89d2955335391e8d
Brodey Newman <brodeynewman@gmail.com>
2018-10-24 22:41:05 -0400

    compact

--- packages/compact/src/index.js
+++ packages/compact/src/index.js
@@ -17,12 +17,19 @@
  * Flattens an array one level deep
  * @param {Array} array - the array to flatten
  * @returns {Array} a newly flattened array
+ * @memberof foldr
+ * @since v0.0.0
+ * @export
+ * @example
+ * compact([1, null, 3, undefined]);       // => [1, 3]
+ * compact(['', 0, 4]);     // => [4]
  */
 function compact(array) {
-  let arr = [], len = array == null ? 0 : array.length, i = -1, ind = 0;
+  let arr = [], len = array == null ? 0 : array.length, i = -1, ind = -1;
   while (++ind < len) {
-    if (!array[ind]) { continue; }
-    arr[++i] = array[ind];
+    if (array[ind]) {
+      arr[++i] = array[ind];
+    }
   }
   return arr;
 }
--- packages/compact/src/index.test.js
+++ packages/compact/src/index.test.js
@@ -2,6 +2,11 @@ const compact = require('./');
 
 describe('compact', () => {
   it('should remove falsy values from an array', () => {
-    expect(true).toBe(false);
+    expect(compact([0, 'hey', 'there', undefined])).toEqual(['hey', 'there']);
+    expect(compact(['', null, 'foo', 'bar'])).toEqual(['foo', 'bar']);
+    expect(compact([{}, 'hey', 'there', null])).toEqual([{}, 'hey', 'there']);
+    expect(compact([0, 'hey', 'there', undefined])).toEqual(['hey', 'there']);
+    expect(compact([true, true, true, true])).toEqual([true, true, true, true]);
+    expect(compact([true, true, false, true])).toEqual([true, true, true]);
   });
 });

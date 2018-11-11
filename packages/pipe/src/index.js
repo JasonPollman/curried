32f06544c95327d4c104b6d339f6c09c529a6289
Brodey Newman <brodeynewman@gmail.com>
2018-11-11 16:13:16 -0500

    jsdoc and review changes

--- packages/pipe/src/index.js
+++ packages/pipe/src/index.js
@@ -11,10 +11,10 @@ const TYPE_ERROR = 'Expected arguments of pipe to be of type function';
 
 /**
  * Takes in functions as parameters, and returns a function
- * that takes in the initial arguements to pass to the first function in the pipe chain.
+ * that takes in the initial arguments to pass to the first function in the pipe chain.
  *
  * After the first function in the pipe chain completes, that value is then passed to the next
- * function in the pipe chain.
+ * function in the pipe chain, and so on.
  * @param {Array<function>} arguments - functions to pipe data through
  * @returns {*} - whatever is returned from the pipe chain
  * @example
@@ -22,7 +22,7 @@ const TYPE_ERROR = 'Expected arguments of pipe to be of type function';
  * const mult = a => a * a;
  *
  * const piped = pipe(add, mult)
- * piped(2, 3) // 25;
+ * piped(2, 3) // => 25;
  *
  */
 function pipe() {
@@ -43,10 +43,8 @@ function pipe() {
     let value = funcs[0].apply(this, arguments);
     let len = 0;
 
-    while (++len < funcs.length) {
-      const func = funcs[len];
-
-      value = func.call(this, value);
+    while (++len < argLen) {
+      value = funcs[len].call(this, value);
     }
 
     return value;
